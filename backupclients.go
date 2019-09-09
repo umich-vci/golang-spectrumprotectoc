3c3cd10666c087fda35182538f9ce5be2368db01
Adam Robinson <adarobin@umich.edu>
2019-09-09 14:15:30 -0400

    add schedules method

--- backupclients.go
+++ backupclients.go
@@ -83,27 +83,43 @@ type UpdateClientRequest struct {
 
 // BackupClientAtRisk contains the elements that make up a backup client at risk response
 type BackupClientAtRisk struct {
-	Server string `json:"SERVER,string,omitempty"`
-	AtRisk string `json:"AT_RISK,string,omitempty"`
-	Link   string `json:"LINK,string,omitempty"`
-	Type   int    `json:"TYPE,int,omitempty"`
-	Name   string `json:"NAME,string,omitempty"`
+	Server string `json:"SERVER"`
+	AtRisk string `json:"AT_RISK"`
+	Link   string `json:"LINK"`
+	Type   int    `json:"TYPE"`
+	Name   string `json:"NAME"`
 }
 
 type backupClientAtRiskRoot struct {
 	ClientAtRisk *BackupClientAtRisk `json:"clientatrisk"`
 }
 
+// BackupClientSchedule contains the elements that make up a backup client schedule response
+type BackupClientSchedule struct {
+	StartTime    string `json:"START_TIME"`
+	ServerName   string `json:"SERVER_NAME"`
+	ScheduleName string `json:"SCHEDULE_NAME"`
+	RunTime      int    `json:"RUNTIME"`
+	DomainName   string `json:"DOMAIN_NAME"`
+}
+
+type backupClientSchedulesRoot struct {
+	ClientSchedules      []BackupClientSchedule `json:"clientschedules"`
+	ClientSchedulesCount int                    `json:"clientschedules_count"`
+}
+
 // BackupClients is an interface for interacting with
 // IBM Spectrum Protect backup clients
 type BackupClients interface {
 	AssignSchedule(ctx context.Context, serverName string, clientName string, scheduleDomain string, scheduleName string) (*http.Response, error)
+	AtRisk(ctx context.Context, serverName string, clientName string) (*BackupClientAtRisk, *http.Response, error)
 	Decommission(ctx context.Context, serverName string, clientName string) (*http.Response, error)
 	DecommissionVM(ctx context.Context, serverName string, clientName string, vmName string) (*http.Response, error)
 	Details(ctx context.Context, serverName string, clientName string) (*BackupClientDetail, *http.Response, error)
 	List(ctx context.Context) ([]BackupClient, *http.Response, error)
 	Lock(ctx context.Context, serverName string, clientName string) (*http.Response, error)
 	RegisterNode(ctx context.Context, serverName string, createRequest *RegisterClientRequest) (*http.Response, error)
+	Schedules(ctx context.Context, serverName string, domain string, clientName string) ([]BackupClientSchedule, *http.Response, error)
 	Unlock(ctx context.Context, serverName string, clientName string) (*http.Response, error)
 	Update(ctx context.Context, serverName string, clientName string, update *UpdateClientRequest) (*http.Response, error)
 	UpdatePassword(ctx context.Context, serverName string, clientName string, password string) (*http.Response, error)
@@ -476,4 +492,34 @@ func (s *BackupClientsOp) AtRisk(ctx context.Context, serverName string, clientN
 	}
 
 	return root.ClientAtRisk, resp, err
+}
+
+// Schedules of a specific backup client
+func (s *BackupClientsOp) Schedules(ctx context.Context, serverName string, domain string, clientName string) ([]BackupClientSchedule, *http.Response, error) {
+	if serverName == "" {
+		return nil, nil, NewArgError("serverName", "cannot be empty")
+	}
+
+	if domain == "" {
+		return nil, nil, NewArgError("domain", "cannot be empty")
+	}
+
+	if clientName == "" {
+		return nil, nil, NewArgError("clientName", "cannot be empty")
+	}
+
+	path := serversBasePath + "/" + serverName + "/domains/" + domain + "/clients/" + clientName + "/schedules"
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(backupClientSchedulesRoot)
+	resp, err := s.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return root.ClientSchedules, resp, err
 }
